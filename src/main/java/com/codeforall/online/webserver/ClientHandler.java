f63febc34197a471f8ac1e9025c5819548d85476
Kvilarinho <katia.vilas@gmail.com>
2025-11-01 15:45:39 +0000

    feat: implement concurrency with thread pool and per-client handler

---
+++ src/main/java/com/codeforall/online/webserver/ClientHandler.java
@@ -0,0 +1,139 @@
+package com.codeforall.online.webserver;
+
+
+import com.codeforall.online.webserver.utils.Header;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static com.codeforall.online.webserver.WebServer.DOC_ROOT;
+import static com.codeforall.online.webserver.WebServer.FILE_404;
+
+public class ClientHandler implements Runnable {
+
+    private final Logger logger;
+    private final Socket client;
+    private BufferedReader reader;
+    private DataOutputStream output;
+    private static final int BUF_SIZE = 1024;
+
+    public ClientHandler(Socket client, Logger logger) {
+        this.client = client;
+        this.logger = logger;
+    }
+
+    @Override
+    public void run() {
+        logger.log(Level.INFO, "new connection from " + getAddress(client));
+        try (Socket s = client) {
+            setUpStreams(s);
+            dispatch();
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "client disconnected " + getAddress(client));
+        }
+    }
+
+
+    private void dispatch() throws IOException {
+        String httpRequest = readHttpRequest();
+
+        if (httpRequest == null || !httpRequest.toUpperCase().startsWith("GET")) {
+            return;
+        }
+
+        String fileName = extractFileRequested(httpRequest);
+        File file = new File(DOC_ROOT, fileName);
+
+
+        logger.log(Level.INFO, "Request received: " + getRequestedLine(httpRequest));
+
+        if (file.exists() && file.isFile()) {
+
+            sendHeader(file);
+            sendFile(file);
+
+        } else {
+            file = FILE_404;
+
+            logger.log(Level.WARNING, "page not found");
+            sendHeader(file);
+            sendFile(file);
+        }
+    }
+
+
+    private String readHttpRequest() throws IOException {
+
+        StringBuilder request = new StringBuilder();
+        String requestLine = "";
+
+        while ((requestLine = reader.readLine()) != null && !requestLine.isEmpty()) {
+            request.append(requestLine);
+            request.append(System.lineSeparator());
+        }
+
+        return request.toString();
+    }
+
+
+    private String extractFileRequested(String httpRequest) {
+
+        String firstLine = getRequestedLine(httpRequest);
+
+        int first = firstLine.indexOf(" ");
+        int second = firstLine.indexOf(" ", first + 1);
+
+        String fileRequested = firstLine.substring(first + 1, second);
+
+        if (fileRequested.equals("/")) {
+            fileRequested = "/index.html";
+        }
+
+        if (fileRequested.startsWith("/")) {
+            fileRequested = fileRequested.substring(1);
+        }
+
+
+        return fileRequested;
+    }
+
+    private String getRequestedLine (String httpRequest) {
+
+        String[] lines = httpRequest.split(System.lineSeparator());
+        String firstLine = lines[0];
+
+        return firstLine;
+    }
+
+    private void sendHeader(File file) throws IOException {
+
+        Header header = new Header(file);
+        output.writeBytes(file == FILE_404 ? header.getHeader404() : header.getHeader200());
+        output.flush();
+    }
+
+    private void sendFile(File file) throws IOException {
+        byte[] buf = new byte[BUF_SIZE];
+        int bytesRead;
+
+        try (FileInputStream input = new FileInputStream(file)) {
+            while ((bytesRead = input.read(buf)) != -1) {
+                output.write(buf, 0, bytesRead);
+
+            }
+            output.flush();
+        }
+    }
+
+    private void setUpStreams(Socket s) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        output = new DataOutputStream(new BufferedOutputStream(client.getOutputStream()));
+    }
+
+    private String getAddress(Socket client) {
+        return client.getInetAddress().getHostAddress() + ": " + client.getPort();
+    }
+
+}
--- src/main/java/com/codeforall/online/webserver/WebServer.java
+++ src/main/java/com/codeforall/online/webserver/WebServer.java
@@ -1,128 +1,48 @@
 package com.codeforall.online.webserver;
 
-import com.codeforall.online.webserver.utils.Header;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class WebServer {
 
     private ServerSocket server;
-    private Socket client;
-    private BufferedReader reader;
-    private DataOutputStream output;
-    private static final String DOC_ROOT = "src/main/www";
+    public static final String DOC_ROOT = "src/main/www";
     public static final File FILE_404 = new File(DOC_ROOT, "404.html");
-    private Header header;
-    private final int BUF_SIZE = 1024;
-
+    private static final Logger LOGGER = Logger.getLogger(WebServer.class.getName());
+    private static final int MAXIMUM_CLIENTS = 306;
+    private final ExecutorService threadPool = Executors.newFixedThreadPool(MAXIMUM_CLIENTS);
 
     public void listen(int port) {
+
         try {
+
             server = new ServerSocket(port);
-            System.out.println("Listening at port " + server.getLocalPort());
+            LOGGER.log(Level.INFO,"Listening at port " + port);
 
-            while (true) {
-                client = server.accept();
-                System.out.println("Accepted client: " + client);
-                setUpStreams();
-                serve();
-                client.close();
+            serve(server);
+
+        } catch (IOException e) {
+
+            LOGGER.log(Level.SEVERE, "could not bind to port: " + port);
+            LOGGER.log(Level.SEVERE, e.getMessage());
+            System.exit(1);
+
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+
+    private void serve(ServerSocket server) throws IOException {
+
+        while (true) {
+
+                Socket client = server.accept();
+                threadPool.submit(new ClientHandler(client, LOGGER));
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void serve() throws IOException {
-        String httpRequest = readHttpRequest();
-
-        if (httpRequest == null || !httpRequest.toUpperCase().startsWith("GET")) {
-            return;
-        }
-
-        String fileName = extractFileRequested(httpRequest);
-        File file = new File(DOC_ROOT, fileName);
-
-        if (file.exists() && file.isFile()) {
-
-            sendHeader(file);
-            sendFile(file);
-
-        } else {
-            file = FILE_404;
-
-            sendHeader(file);
-            sendFile(file);
-        }
-    }
-
-    private String readHttpRequest() throws IOException {
-
-        StringBuilder request = new StringBuilder();
-        String requestLine = "";
-
-        while ((requestLine = reader.readLine()) != null && !requestLine.isEmpty()) {
-            request.append(requestLine);
-            request.append(System.lineSeparator());
-        }
-
-        return request.toString();
-    }
-
-    private String extractFileRequested(String httpRequest) {
-        String[] lines = httpRequest.split(System.lineSeparator());
-        String requestedLine = lines[0];
-
-        int first = requestedLine.indexOf(" ");
-        int second = requestedLine.indexOf(" ", first + 1);
-
-        String fileRequested = requestedLine.substring(first + 1, second);
-
-        if (fileRequested.equals("/")) {
-            fileRequested = "/index.html";
-        }
-
-        if (fileRequested.startsWith("/")) {
-            fileRequested = fileRequested.substring(1);
-        }
-
-
-        System.out.println(fileRequested);
-        return fileRequested;
-    }
-
-    private void sendHeader(File file) throws IOException {
-
-        header = new Header(file);
-        output.writeBytes(file == FILE_404 ? header.getHeader404() : header.getHeader200());
-        output.flush();
-    }
-
-    private void sendFile(File file) throws IOException {
-        byte[] buf = new byte[BUF_SIZE];
-        int bytesRead;
-        long total = 0;
-
-        FileInputStream input = new FileInputStream(file);
-        while ((bytesRead = input.read(buf)) != -1) {
-            output.write(buf, 0, bytesRead);
-            total += bytesRead;
-        }
-
-        output.flush();
-
-    }
-
-    private void setUpStreams() {
-        try {
-            reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            output = new DataOutputStream(new BufferedOutputStream(client.getOutputStream()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 }
